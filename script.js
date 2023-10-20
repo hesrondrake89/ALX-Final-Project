dd8fd5f4974d372e0356dab81d7aef677c36dc6f
hesrondrake89 <113859517+hesrondrake89@users.noreply.github.com>
2023-10-20 11:19:46 +0100

    Updated the comments for the even listener and all other important code function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const tmdbApiKey = 'fc90ff78fa17b85bf5bcbdd19e6c16c5'; 
-const omdbApiKey = '8a8cd539'; 
+const tmdbApiKey = 'fc90ff78fa17b85bf5bcbdd19e6c16c5'; // TMDB API Key
+const omdbApiKey = '8a8cd539'; // OMBD API Key
 
-// Function to fetch and display movie data from OMDB API
+// This is the function to fetch and display movie data from OMDB API
 async function fetchMovieData(title) {
     try {
         const response = await fetch(`https://www.omdbapi.com/?t=${title}&apikey=${omdbApiKey}`);
@@ -22,7 +22,7 @@ async function fetchMovieData(title) {
             params.set('rating', data.imdbRating);
             params.set('poster', data.Poster);
 
-            // Construct the URL with parameters for movie_info.html
+            // This function construct the URL with parameters for movie_info.html
             const movieInfoURL = `movie_info.html?${params.toString()}`;
 
             // Redirect to the new page
@@ -33,7 +33,7 @@ async function fetchMovieData(title) {
     }
 }
 
-// Function to fetch and display upcoming movies from TMDb API
+// This is the function to fetch and display upcoming movies from TMDb API
 async function fetchUpcomingMovies() {
     try {
     const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${tmdbApiKey}&language=en-US&page=1`);
@@ -56,7 +56,7 @@ async function fetchUpcomingMovies() {
             movieTitle.textContent = movie.title;
             movieTitle.style.cursor = 'pointer';
 
-            // Add a click event to the movie title to fetch and display movie information from OMDB API
+            // This function adds a click event to the movie title to fetch and display movie information from OMDB API
             movieTitle.addEventListener('click', function () {
                 fetchMovieDataFromOMDB(movie.title);
             });
@@ -78,29 +78,29 @@ async function fetchUpcomingMovies() {
     }
 }
 
-// Call the function to fetch and display upcoming movies when the page loads
+// This calls the function to fetch and display upcoming movies when the page loads
 document.addEventListener('DOMContentLoaded', function () {
     fetchUpcomingMovies();
 });
 
-// Search button click event
+// This is the search button click event
 document.getElementById('fetchButton').addEventListener('click', () => {
     const movieTitle = document.getElementById('movieTitleInput').value;
 
-    // Check if the input is empty
+    // This code checks if the input is empty
     if (!movieTitle) {
-        // Display an error message
+        // This displays an error message if the input is empty
         const errorMessage = 'Title is required. Please enter a movie or series title.';
         document.getElementById('errorMessage').textContent = errorMessage;
     } else {
-        // Clear any previous error message
+        // This clears any previous errors
         document.getElementById('errorMessage').textContent = '';
-        // Call the function to fetch movie data
+        // This calls the function to fetch movie data
         fetchMovieData(movieTitle);
     }
 });
 
-// Function to fetch and display upcoming movies from TMDb API
+// This is the function to fetch and display upcoming movies from TMDb API
 async function fetchUpcomingMovies() {
     try {
         const response = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${tmdbApiKey}&language=en-US&page=1`);
@@ -108,7 +108,7 @@ async function fetchUpcomingMovies() {
 
         if (data.results) {
             const movieContainer = document.querySelector('.movie-container');
-            movieContainer.innerHTML = ''; // Clear existing content
+            movieContainer.innerHTML = ''; // This code clears existing content
 
             data.results.slice(0, 30).forEach(movie => {
                 const movieElement = document.createElement('div');
@@ -122,7 +122,7 @@ async function fetchUpcomingMovies() {
                 movieTitle.textContent = movie.title;
                 movieTitle.style.cursor = 'pointer';
 
-                // Add a click event to the movie title to fetch and display movie information from OMDB API
+                // This function adds a click event to the movie title to fetch and display movie information from OMDB API
                 movieTitle.addEventListener('click', function () {
                     fetchMovieDataFromOMDB(movie.title);
                 });
@@ -140,15 +140,15 @@ async function fetchUpcomingMovies() {
     }
 }
 
-// Function to fetch and display movie information from OMDB API
+// This function fetches and display movie information from OMDB API when a user clicks the title of the upcoming movie releases
 async function fetchMovieDataFromOMDB(title) {
     try {
         const response = await fetch(`https://www.omdbapi.com/?t=${title}&apikey=${omdbApiKey}`);
         const data = await response.json();
 
         if (data.Response === 'True') {
-            // Handle the OMDB API response to display movie details
-            // Construct URL parameters for movie details
+            // This section handles the OMDB API response to display movie details
+            // this section construct URL parameters for movie details
             const params = new URLSearchParams();
             params.set('title', data.Title);
             params.set('year', data.Year);
@@ -160,13 +160,13 @@ async function fetchMovieDataFromOMDB(title) {
             params.set('rating', data.imdbRating);
             params.set('poster', data.Poster);
 
-            // Construct the URL with parameters for movie_info.html
+            // This section constructs the URL with parameters for movie_info.html file
             const movieInfoURL = `movie_info.html?${params.toString()}`;
 
-            // Redirect to the new page to display movie information
+            // This function redirects to the movie_info.html page file to display movie information
             window.location.href = movieInfoURL;
         } else {
-            // Handle the case when the movie is not found in OMDB
+            // This setion handles the case when the movie is not found in OMDB
             console.error('Movie not found:', data.Error);
         }
     } catch (error) {
@@ -174,17 +174,17 @@ async function fetchMovieDataFromOMDB(title) {
     }
 }
 
-// Call the function to fetch and display upcoming movies when the page loads
+// This section calls the function to fetch and display upcoming movies when the page loads
 document.addEventListener('DOMContentLoaded', function () {
     fetchUpcomingMovies();
 });
 
-// Add an event listener to run the code when the page loads
+// Added an event listener to run the code when the page loads
 window.addEventListener('load', function() {
-    // Select the container element that you want to scroll
+    // this section selects the container element that you want to scroll
     var container = document.querySelector('.movie-scroll-container');
     
-    // Set the initial scroll position to -100% to start at the first movie
+    // this sets the initial scroll position to -100% to start at the first movie
     container.scrollLeft = container.scrollWidth;
 });
 
